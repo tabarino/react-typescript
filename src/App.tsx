70faa20a1a5b1c12c783d1e42c66423382d77871
Ivan Tabarino <tabarino@outlook.com>
2021-05-22 15:52:12 +0100

    Remove a TodoItem

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,16 @@ function App() {
     });
   };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    });
+  };
+
   return (
     <div className="App">
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './TodoItem.module.css';
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
   return (
-    <li className={styles.item}>{props.text}</li>
+    <li className={styles.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
   );
 };
 
